ef0f51720b773e031fea4302b90a9eca8c8efd57
cmplx <22222976+Complexicon@users.noreply.github.com>
2021-03-26 22:18:43 +0100

    make context installable

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,29 @@ const char* GetClipboardText() {
 
 int main(int argc, char const* argv[]) {
 
+	if(argc == 2 && cmplx::strcmp(argv[1], "install-context")) {
+
+		if(MessageBoxA(0, "Install fastclone into Contextmenu?", "fastclone v1", MB_YESNO | MB_ICONQUESTION) == IDNO) return 0;
+
+		HKEY shell;
+		RegOpenKeyExA(HKEY_CURRENT_USER, "SOFTWARE\\Classes\\Directory\\Background\\shell", 0, KEY_ALL_ACCESS, &shell);
+
+		HKEY contextKey;
+		RegCreateKeyA(shell, "fastclone", &contextKey);
+		RegSetValueExA(contextKey, "Extended", 0, REG_SZ, (LPBYTE) "", 1);
+		RegSetValueExA(contextKey, 0, 0, REG_SZ, (LPBYTE) "Fastclone (Git)", 15);
+
+		HKEY command;
+		RegCreateKeyA(contextKey, "command", &command);
+
+		char buf[MAX_PATH];
+		GetModuleFileNameA(0, buf, MAX_PATH);
+
+		RegSetValueExA(command, 0, 0, REG_SZ, (LPBYTE)buf, cmplx::strlen(buf) + 1);
+
+		return 0;
+	}
+
 	Str cd;
 
 	if(argc == 2) cd = argv[1];
